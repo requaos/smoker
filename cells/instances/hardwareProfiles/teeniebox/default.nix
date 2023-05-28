5f1143910eca56342a12d3673f2510fc4b69a2ca
Neil Skinner <reqpro@requaos.com>
2023-05-28 18:58:47 +0000

    feat: add extra modprobe config for nested virtualization

--- cells/instances/hardwareProfiles/teeniebox/default.nix
+++ cells/instances/hardwareProfiles/teeniebox/default.nix
@@ -10,11 +10,22 @@
     # bleeding-edge kernel:
     kernelPackages = inputs.nixpkgs.linuxPackages_testing;
 
-    kernelModules = ["kvm-intel"];
+    # nested virtualization in qemu/kvm
+    extraModprobeConfig = "options kvm_intel nested=1";
+
+    # The touchpad in Dell 13" XPS 9320 confuses the 'psmouse'
+    # module and causes excessive delay and potential freezing
+    # on shutdown.
     blacklistedKernelModules = ["psmouse"];
+
+    # virtualization module
+    kernelModules = ["kvm-intel"];
+
+    # graphics fix from dell for 'iris' opengl support
     kernelParams = [
       "i915.force_probe=46a6"
     ];
+
     initrd = {
       availableKernelModules = [
         "nvme"
@@ -22,7 +33,15 @@
         "vmd"
         "thunderbolt"
       ];
-      luks.devices."root".device = "/dev/disk/by-uuid/cc582743-c00f-457a-bea0-841a72b945ec";
+
+      # root disk encryption
+      luks = {
+        devices = {
+          "root" = {
+            device = "/dev/disk/by-uuid/cc582743-c00f-457a-bea0-841a72b945ec";
+          };
+        };
+      };
     };
 
     # used for cross-compiling for aarch64.
