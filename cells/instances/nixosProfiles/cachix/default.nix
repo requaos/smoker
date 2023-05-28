c158f887ad6a8019f0ac8561033c6a3edd3148ee
Neil Skinner <reqpro@requaos.com>
2023-05-28 02:52:31 +0000

    feat: consolidate cachix definition

--- cells/instances/nixosProfiles/cachix/default.nix
+++ cells/instances/nixosProfiles/cachix/default.nix
@@ -6,10 +6,12 @@
         "https://cache.nixos.org/"
         "https://nrdxp.cachix.org"
         "https://nix-community.cachix.org"
+        "https://colmena.cachix.org"
       ];
       trusted-public-keys = [
         "nrdxp.cachix.org-1:Fc5PSqY2Jm1TrWfm88l6cvGWwz3s93c6IOifQWnhNW4="
         "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "colmena.cachix.org-1:7BzpDnjjH8ki2CT3f6GdOk7QAzPOl+1t3LvTLXqYcSg="
       ];
     };
   };
--- flake.nix
+++ flake.nix
@@ -3,18 +3,6 @@
 
   nixConfig = {
     extra-experimental-features = "nix-command flakes";
-
-    extra-substituters = [
-      # nix-community
-      "https://nix-community.cachix.org"
-      # colmena
-      "https://colmena.cachix.org"
-    ];
-
-    extra-trusted-public-keys = [
-      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
-      "colmena.cachix.org-1:7BzpDnjjH8ki2CT3f6GdOk7QAzPOl+1t3LvTLXqYcSg="
-    ];
   };
 
   inputs = {
