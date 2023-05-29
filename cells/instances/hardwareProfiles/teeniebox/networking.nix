0b56f28815d2025b8939a848ea8bae21c64dbac9
Neil Skinner <reqpro@requaos.com>
2023-05-29 22:55:32 +0000

    fix: remove broken bridge network interface configuration

--- cells/instances/hardwareProfiles/teeniebox/networking.nix
+++ cells/instances/hardwareProfiles/teeniebox/networking.nix
@@ -6,11 +6,4 @@
   networkmanager = {
     enable = cell.lib.mkForce true;
   };
-
-  # bridge adapter for host-to-vm network visibility in libvirtd
-  bridges = {
-    "br0" = {
-      interfaces = ["wlp0s20f3"];
-    };
-  };
 }
