80f6ce6518f4c579fa7fce2dc5e4e9ce3e395e0e
Neil Skinner <reqpro@requaos.com>
2023-06-02 22:41:25 +0000

    feat: try using overlay to coerse gl stuff from the same dep family

--- cells/instances/hardwareProfiles/teeniebox/hardware.nix
+++ cells/instances/hardwareProfiles/teeniebox/hardware.nix
@@ -14,8 +14,24 @@
     platform = "ipu6ep";
   };
 
+  inputs.nixpkgs.overlays = [
+    (_: _: {
+      # !!! overlay to force full deps on unstable opengl
+      mesa = unstable.mesa;
+      intel-media-driver = unstable.intel-media-driver;
+      vaapiIntel = unstable.vaapiIntel;
+      vaapiVdpau = unstable.vaapiVdpau;
+      libvdpau-va-gl = unstable.libvdpau-va-gl;
+      virglrenderer = unstable.virglrenderer;
+      xdg-desktop-portal-wlr = unstable.xdg-desktop-portal-wlr;
+      qemu = unstable.qemu;
+    })
+  ];
+
   opengl = {
     enable = true;
+    driSupport = true; # for wine with openGL
+    driSupport32Bit = true; # for wine with openGL
     extraPackages = with inputs.nixpkgs; [
       intel-media-driver # LIBVA_DRIVER_NAME=iHD
       vaapiIntel # LIBVA_DRIVER_NAME=i965 (older but works better for Firefox/Chromium)
