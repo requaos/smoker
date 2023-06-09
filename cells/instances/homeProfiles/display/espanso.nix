6668fc25c44eb762ba5d51728651bf1a73938632
Neil Skinner <reqpro@requaos.com>
2023-06-09 19:42:32 -0400

    chore: espanso configuration changes

--- cells/instances/homeProfiles/display/espanso.nix
+++ cells/instances/homeProfiles/display/espanso.nix
@@ -4,40 +4,41 @@
 }: {
   services.espanso = {
     enable = true;
-    settings = {
-      toggle_key = "META";
-      matches = [
-        {
-          trigger = ":shrug";
-          replace = ''¯\_(ツ)_/¯'';
-        }
-        {
-          trigger = ":date";
-          replace = ''{{output}}'';
-          vars = [
-            {
-              name = "output";
-              type = "date";
-              params = {
-                format = "%Y-%m-%d";
-              };
-            }
-          ];
-        }
-        {
-          trigger = ":now";
-          replace = ''{{output}}'';
-          vars = [
-            {
-              name = "output";
-              type = "date";
-              params = {
-                format = "%Y-%m-%dT%H-%M-%S";
-              };
-            }
-          ];
-        }
-      ];
+    matches = {
+      "META" = {
+        matches = [
+          {
+            trigger = ":shrug";
+            replace = ''¯\_(ツ)_/¯'';
+          }
+          {
+            trigger = ":date";
+            replace = ''{{output}}'';
+            vars = [
+              {
+                name = "output";
+                type = "date";
+                params = {
+                  format = "%Y-%m-%d";
+                };
+              }
+            ];
+          }
+          {
+            trigger = ":now";
+            replace = ''{{output}}'';
+            vars = [
+              {
+                name = "output";
+                type = "date";
+                params = {
+                  format = "%Y-%m-%dT%H-%M-%S";
+                };
+              }
+            ];
+          }
+        ];
+      };
     };
   };
 }
