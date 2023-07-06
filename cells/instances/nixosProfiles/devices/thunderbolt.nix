7eb59cd80d69e837b2ddcd05d4b27e39bf213196
Neil Skinner <reqpro@requaos.com>
2023-07-06 18:58:30 -0400

    fix: add udev rule to authorize thunderbolt devices when connected

--- cells/instances/nixosProfiles/devices/thunderbolt.nix
+++ cells/instances/nixosProfiles/devices/thunderbolt.nix
@@ -4,7 +4,14 @@
 }: let
   inherit (inputs) nixpkgs;
 in {
-  services.hardware.bolt.enable = true;
+  services = {
+    hardware.bolt.enable = true;
+    udev.extraRules = ''
+      # Always authorize thunderbolt connections when they are plugged in.
+      # This is to make sure the USB hub of Thunderbolt is working.
+      ACTION=="add", SUBSYSTEM=="thunderbolt", ATTR{authorized}=="0", ATTR{authorized}="1"
+    '';
+  };
   environment.systemPackages = with nixpkgs; [
     bolt
   ];
