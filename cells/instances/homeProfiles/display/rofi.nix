2e4cf0edcc5df2f7c5eebb52f761ed87de659481
Neil Skinner <reqpro@requaos.com>
2023-08-10 09:37:08 -0400

    drun theme in rofi

--- cells/instances/homeProfiles/display/rofi.nix
+++ cells/instances/homeProfiles/display/rofi.nix
@@ -3,10 +3,13 @@
   cell,
 }: let
   inherit (inputs) nixpkgs;
+  inherit (inputs.home-manager.lib.homeManagerConfiguration.lib.formats.rasi) mkLiteral;
 in {
   programs.rofi = {
     enable = true;
     terminal = "${nixpkgs.alacritty}/bin/alacritty";
-    # theme = {};
+    theme = {
+      "@import" = "${nixpkgs.rofi-unwrapped}/share/rofi/themes/DarkBlue.rasi";
+    };
   };
 }
