e16078bf1d0d9c7cfc195fcfb7e5efbc708f9630
Neil Skinner <req@requaos.com>
2023-11-14 18:06:49 -0500

    rust-rover

--- cells/instances/nixosProfiles/development-rust/environment.nix
+++ cells/instances/nixosProfiles/development-rust/environment.nix
@@ -1,13 +1,13 @@
-{ inputs
-, cell
-,
+{
+  inputs,
+  cell,
 }: {
   systemPackages = with builtins.removeAttrs
-    (
-      inputs.fenix.packages.latest
-      // { inherit (inputs.fenix.packages) rust-analyzer targets; }
-    )
-    [ "withComponents" ]; [
+  (
+    inputs.fenix.packages.latest
+    // {inherit (inputs.fenix.packages) rust-analyzer targets;}
+  )
+  ["withComponents"]; [
     cargo
     clippy
     rust-src
@@ -19,11 +19,13 @@
     # Necessary for the openssl-sys crate:
     inputs.nixpkgs.openssl_1_1
     inputs.nixpkgs.pkg-config
+
+    inputs.nixpkgs.jetbrains.rust-rover
   ];
   sessionVariables = {
     RUST_SRC_PATH = "${inputs.fenix.packages.latest.rust-src}";
     PKG_CONFIG_PATH = "${inputs.nixpkgs.openssl_1_1.dev}/lib/pkgconfig";
     OPENSSL_DIR = "${inputs.nixpkgs.openssl_1_1}";
-    LD_LIBRARY_PATH = cell.lib.makeLibraryPath [ inputs.nixpkgs.openssl_1_1 ];
+    LD_LIBRARY_PATH = cell.lib.makeLibraryPath [inputs.nixpkgs.openssl_1_1];
   };
 }
