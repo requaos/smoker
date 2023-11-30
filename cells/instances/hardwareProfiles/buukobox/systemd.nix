090f61206bc7191db1f9281477d3c1d0fc684f18
Neil Skinner <req@requaos.com>
2023-11-30 01:56:15 -0500

    fixed start level for systemd screen script

--- cells/instances/hardwareProfiles/buukobox/systemd.nix
+++ cells/instances/hardwareProfiles/buukobox/systemd.nix
@@ -4,11 +4,15 @@
 }: {
   user.services.displaySetupOnBoot = with inputs.nixpkgs; {
     description = "Detect displays and set position, gamma and brightness";
+    path = with inputs.nixpkgs; [
+      bash
+      gawk
+      gnugrep
+      xorg.xrandr
+      brightnessctl
+    ];
     script = ''
-      #!${bash}/bin/bash
-
-      CONNECTED_DISPLAYS="$(${xorg.xrandr}/bin/xrandr | ${gnugrep}/bin/grep "\sconnected" | ${gawk}/bin/awk '{ print $1 }')"
-      CDL=($CONNECTED_DISPLAYS)
+      CONNECTED_DISPLAYS="$(xrandr | grep "\sconnected" | awk '{ print $1 }')"
       DOCK_LEFT="DVI-I-2-2"
       DLF="0"
       DOCK_CENTER="HDMI-0"
@@ -17,14 +21,15 @@
       DRF="0"
 
       LOG="Connected Displays:"
-      for CD in $CDL
+      for CD in $CONNECTED_DISPLAYS
       do
               LOG+="\n  - "
               if [ "$CD" == "$DOCK_CENTER" ]
               then
-                      ${xorg.xrandr}/bin/xrandr --output HDMI-0 --brightness 1 --gamma 0.7:0.7:0.7
+                      xrandr --output HDMI-0 --brightness 1 --gamma 0.7:0.7:0.7
+                      sleep 0.2
                       DCF="1"
-                      ${xorg.xrandr}/bin/xrandr 2>&1 >/dev/null
+                      xrandr 2>&1 >/dev/null
                       LOG+="Center";
               elif [ "$CD" == "$DOCK_RIGHT" ]
               then
@@ -42,29 +47,32 @@
       if [[ "$DCF" == "1" && "$DRF" == "1" ]]
       then
               LOG+="\nSetting DVI-I-1-1 to the Right"
-              ${xorg.xrandr}/bin/xrandr --output DVI-I-1-1 --mode 2560x1440 --right-of HDMI-0 --auto --brightness 0.6 --gamma 0.7:0.7:0.7
-              ${xorg.xrandr}/bin/xrandr 2>&1 >/dev/null;
+              xrandr --output DVI-I-1-1 --mode 2560x1440 --right-of HDMI-0 --auto --brightness 0.6 --gamma 0.7:0.7:0.7
+              sleep 0.2
+              xrandr 2>&1 >/dev/null;
       fi
 
       if [[ "$DCF" == "1" && "$DLF" == "1" ]]
       then
               LOG+="\nSetting DVI-I-2-2 to the Left"
-              ${xorg.xrandr}/bin/xrandr --output DVI-I-2-2 --mode 2560x1440 --left-of HDMI-0 --auto --brightness 0.6 --gamma 0.7:0.7:0.7
-              ${xorg.xrandr}/bin/xrandr 2>&1 >/dev/null;
+              xrandr --output DVI-I-2-2 --mode 2560x1440 --left-of HDMI-0 --auto --brightness 0.6 --gamma 0.7:0.7:0.7
+              sleep 0.2
+              xrandr 2>&1 >/dev/null;
       fi
 
       if [ "$DLF" == "1" ]
       then
               LOG+="\nSetting DP-4 to the Bottom"
-              ${xorg.xrandr}/bin/xrandr --output DP-4 --below DVI-I-2-2 --auto --brightness 1 --gamma 0.7:0.7:0.7;
+              xrandr --output DP-4 --below DVI-I-2-2 --auto --brightness 1 --gamma 0.7:0.7:0.7;
       else
               LOG+="\nSetting DP-4"
-              ${xorg.xrandr}/bin/xrandr --output DP-4 --auto --brightness 1 --gamma 0.7:0.7:0.7;
+              xrandr --output DP-4 --auto --brightness 1 --gamma 0.7:0.7:0.7;
       fi
-      ${brightnessctl}/bin/brightnessctl --class=backlight s 40% 2>&1 >/dev/null
+      brightnessctl --class=backlight s 40% 2>&1 >/dev/null
 
       printf "$LOG";
     '';
-    wantedBy = ["multi-user.target"]; # starts after login
+    wantedBy = ["graphical-session.target"];
+    partOf = ["graphical-session.target"];
   };
 }
