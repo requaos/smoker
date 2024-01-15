0a68283dbc9363608d356287353577eb8f57c570
Neil Skinner <req@requaos.com>
2024-01-15 09:31:21 -0500

    add tool dependencies

--- cells/instances/nixosProfiles/devices-android/environment.nix
+++ cells/instances/nixosProfiles/devices-android/environment.nix
@@ -16,6 +16,11 @@ in {
               sha256 = "sha256-0aozmQ4Eb5zL4rtNHSFjEynfObUkYlid1PgMDVmRkwY=";
             };
             doCheck = false;
+            propagatedBuildInputs = [
+              # Specify dependencies
+              nixpkgs.python3Packages.pycryptodome
+              nixpkgs.python3Packages.libusb1
+            ];
           }
         )
       ]))
