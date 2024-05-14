d4e26417bd3eb5eabf1a54f6700a9a1f5448c147
Neil Skinner <req@requaos.com>
2024-05-14 09:38:25 -0400

    try add session default

--- cells/instances/nixosProfiles/display-awesome/services.nix
+++ cells/instances/nixosProfiles/display-awesome/services.nix
@@ -1,7 +1,9 @@
 {
   inputs,
   cell,
-}: {
+}: let
+  inherit (inputs) nixpkgs;
+in {
   displayManager = {
     defaultSession = "none+awesome";
     autoLogin = {
@@ -9,10 +11,29 @@
       enable = true;
     };
   };
-  xserver.displayManager.lightdm = {
-    greeter.enable = false;
-    autoLogin.timeout = 0;
-    enable = true;
+  xserver = {
+    windowManager.awesome = {
+      enable = true;
+      luaModules = with nixpkgs.luaPackages; [
+        luarocks # is the package manager for Lua modules
+        luadbi-mysql # Database abstraction layer
+      ];
+    };
+    displayManager = {
+      lightdm = {
+        greeter.enable = false;
+        autoLogin.timeout = 0;
+        enable = true;
+      };
+      # We may need to manually create a session:
+      # session = [
+      #   {
+      #     name = "i3";
+      #     manage = "desktop";
+      #     start = ''exec $HOME/.xsession'';
+      #   }
+      # ];
+    };
   };
   tumbler.enable = true;
   gvfs.enable = true;
