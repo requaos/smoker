8659309ada4e2e0ea1253954f5f32cad57f3fdbc
Neil Skinner <req@requaos.com>
2024-05-14 09:44:41 -0400

    add session for awesomewm and set as default, allow windowManager to be configured via home-manager

--- cells/instances/nixosProfiles/display-awesome/services.nix
+++ cells/instances/nixosProfiles/display-awesome/services.nix
@@ -5,34 +5,26 @@
   inherit (inputs) nixpkgs;
 in {
   displayManager = {
-    defaultSession = "none+awesome";
+    defaultSession = "awesome";
     autoLogin = {
       user = "nixos";
       enable = true;
     };
   };
   xserver = {
-    windowManager.awesome = {
-      enable = true;
-      luaModules = with nixpkgs.luaPackages; [
-        luarocks # is the package manager for Lua modules
-        luadbi-mysql # Database abstraction layer
-      ];
-    };
     displayManager = {
       lightdm = {
         greeter.enable = false;
         autoLogin.timeout = 0;
         enable = true;
       };
-      # We may need to manually create a session:
-      # session = [
-      #   {
-      #     name = "i3";
-      #     manage = "desktop";
-      #     start = ''exec $HOME/.xsession'';
-      #   }
-      # ];
+      session = [
+        {
+          name = "awesome";
+          manage = "desktop";
+          start = ''exec $HOME/.xsession'';
+        }
+      ];
     };
   };
   tumbler.enable = true;
